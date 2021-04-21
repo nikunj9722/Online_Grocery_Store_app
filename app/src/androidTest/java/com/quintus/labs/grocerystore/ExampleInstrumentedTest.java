49a8b5002e6b69ae230eb7545bf7d10389a0a808
np2312 <68021717+np2312@users.noreply.github.com>
2021-04-21 14:59:25 +0530

    Commit

---
+++ app/src/androidTest/java/com/quintus/labs/grocerystore/ExampleInstrumentedTest.java
@@ -0,0 +1,16 @@
+package com.quintus.labs.grocerystore;
+
+import androidx.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+
+    }
+}
---
+++ app/src/main/java/com/quintus/labs/grocerystore/interfaces/AddorRemoveCallbacks.java
@@ -0,0 +1,12 @@
+package com.quintus.labs.grocerystore.interfaces;
+
+
+
+public interface AddorRemoveCallbacks {
+
+    void onAddProduct();
+
+    void onRemoveProduct();
+
+    void updateTotalPrice();
+}
